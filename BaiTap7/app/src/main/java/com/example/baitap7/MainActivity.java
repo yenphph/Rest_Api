1692e337f304bc36b88042aeffe4c6833e98ba30
Hoang Yen <yenphph34781@fpoly.edu.vn>
2024-03-20 09:01:42 +0700

    yenphph34781

--- BaiTap7/app/src/main/java/com/example/baitap7/MainActivity.java
+++ BaiTap7/app/src/main/java/com/example/baitap7/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.recyclerView);
-        itemList = generateItemList(); // Phương thức này để tạo danh sách các mục
+        itemList = generateItemList();
         adapter = new Adapter(itemList);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -32,19 +32,19 @@ public class MainActivity extends AppCompatActivity {
         fabAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Điều hướng sang activity mới khi click vào FAB
+
                 startActivity(new Intent(MainActivity.this, Add_Update.class));
             }
         });
     }
 
-    // Phương thức này tạo danh sách các mục (chỉ để minh họa)
+
     private List<Clothes> generateItemList() {
         List<Clothes> list = new ArrayList<>();
         list.add(new Clothes("Type1", "Name1", 10.0));
         list.add(new Clothes("Type2", "Name2", 20.0));
         list.add(new Clothes("Type3", "Name3", 30.0));
-        // Thêm các mục khác nếu cần
+
         return list;
     }
 }
